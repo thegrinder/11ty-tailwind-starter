9fc474231089579fa2621f98f37eab312b48f00a
thegrinder <paul.szczepkowski@gmail.com>
2022-01-30 17:48:00 +0100

    Set up work section

---
+++ src/_data/jobs.js
@@ -0,0 +1,50 @@
+module.exports = [
+  {
+    company: 'Cooltra',
+    logo: '/public/cooltra.png',
+    startDate: 'Jun 2021',
+    endDate: 'till now',
+    items: [],
+  },
+  {
+    company: 'Docplanner',
+    logo: '/public/docplanner.jpeg',
+    startDate: 'Sep 2020',
+    endDate: 'June 2021',
+    items: [],
+  },
+  {
+    company: 'Zopa',
+    logo: '/public/zopa.jpeg',
+    startDate: 'Aug 2018',
+    endDate: 'Aug 2020',
+    items: [
+      'Developing credit card web origination journey',
+      'Building credit card module for the React Native app',
+      'Heavily contributing to Zopa open source components library',
+      'Technologies: Typescript, React, React Native, Redux, Jest, Cypress, Webpack,',
+    ],
+  },
+  {
+    company: 'Strands',
+    logo: '/public/strands.jpeg',
+    startDate: 'May 2017',
+    endDate: 'Aug 2018',
+    items: [
+      'Architecting components library used in all Strands products',
+      'Implementing brand new invoicing widget',
+      'Technologies: React, Redux, Webpack, Immutable, ES6/ES7',
+    ],
+  },
+  {
+    company: 'Whisbi',
+    logo: '/public/whisbi.png',
+    startDate: 'Mar 2016',
+    endDate: 'Apr 2017',
+    items: [
+      'Redesigning and maintaining of clients’ landing pages',
+      'Improving company Wordpress website',
+      'Technologies: Javascript, jQuery, Sass, Handlebars, Gulp, git',
+    ],
+  },
+];
