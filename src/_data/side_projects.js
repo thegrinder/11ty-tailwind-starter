e7e0fc1fdce03d28f073600163379294bdab0e13
thegrinder <paul.szczepkowski@gmail.com>
2022-01-31 20:48:13 +0100

    Add one side project

---
+++ src/_data/side_projects.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    company: 'Asociación Abejas Silvestres',
+    logo: '/public/logos/abejas-silvestres.svg',
+    startDate: 'Oct 2021',
+    endDate: 'Jan 2022',
+    items: [
+      'Redesigning old website',
+      'Restructuring and organizing content',
+      'Building a brand new website from scratch',
+    ],
+    stack: ['React', 'Next.js', 'Tailwind CSS'],
+  },
+];
