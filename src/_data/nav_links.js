404ff912c78d2a7d09a4499d25c15b853d1d0239
thegrinder <paul.szczepkowski@gmail.com>
2022-02-04 19:35:19 +0100

    Refactor footer and navbar markup

---
+++ src/_data/nav_links.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    label: 'P.',
+    href: '/',
+    id: 'home',
+  },
+  {
+    label: 'Blog',
+    href: '/blog',
+    id: 'blog',
+  },
+];
---
+++ src/_data/social_links.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    label: 'Github profile',
+    href: 'https://github.com/thegrinder',
+    icon: 'github',
+  },
+  {
+    label: 'Linkedin profile',
+    href: 'https://www.linkedin.com/in/pawelszczepkowski',
+    icon: 'linkedin',
+  },
+];
